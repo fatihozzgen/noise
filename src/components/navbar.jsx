4f6824c0b9193f3a09467cf73305ed13170f1fc3
fatihozzgen <fatihozzgen@gmail.com>
2022-12-26 22:43:43 +0300

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../style/navbar.css";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { MdFavoriteBorder } from "react-icons/md";
+import { BsBasket } from "react-icons/bs";
+
+function navbar() {
+  return (
+    <div className="navbar">
+      <div className="nav-logo"> NOISE</div>
+
+      <div className="navigation">
+        <div> HOME</div>
+        <div> MEN'S</div>
+        <div> WOMEN'S</div>
+        <div> KID'S</div>
+        <div> ACCESSORIES</div>
+      </div>
+
+      <div className="nav-right-side">
+        <div>Login/Register</div>
+        <BiSearchAlt2 />
+        <MdFavoriteBorder />
+        <BsBasket />
+      </div>
+    </div>
+  );
+}
+
+export default navbar;
