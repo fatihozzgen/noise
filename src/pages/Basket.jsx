cd19390cd009bca467cc1d08a75740ea2fed1a0f
fatihozzgen <fatihozzgen@gmail.com>
2023-01-03 14:40:35 +0300

    basket page upgrade

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Cloth2 from "../images/cloth2.jpg";
 
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { VscDiffAdded, VscDiffRemoved } from "react-icons/vsc";
+
 function Basket() {
   return (
     <>
@@ -23,14 +27,25 @@ function Basket() {
               <div className="basket-desc">
                 <div className="basket-name">Slim Fit Jeans</div>
                 <div className="delete-fav">
-                  <span>Remove item</span> <span>Remove item</span>
+                  <RiDeleteBin5Line />
+                  Remove item
+                  <MdOutlineFavoriteBorder />
+                  Move to favorite
                 </div>
               </div>
             </div>
             <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
+            <div className="basket-color">L</div>
+            <div className="basket-amount">
+              <div className="amount-svg">
+                <VscDiffAdded />
+              </div>
+              1
+              <div className="amount-svg">
+                <VscDiffRemoved />
+              </div>
+            </div>
+            <div className="basket-price">249.99 $</div>
           </div>
 
           <div className="basket-cards">
@@ -39,14 +54,25 @@ function Basket() {
               <div className="basket-desc">
                 <div className="basket-name">Slim Fit Jeans</div>
                 <div className="delete-fav">
-                  <span>Remove item</span> <span>Remove item</span>
+                  <RiDeleteBin5Line />
+                  Remove item
+                  <MdOutlineFavoriteBorder />
+                  Move to favorite
                 </div>
               </div>
             </div>
             <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
+            <div className="basket-color">32</div>
+            <div className="basket-amount">
+              <div className="amount-svg">
+                <VscDiffAdded />
+              </div>
+              1
+              <div className="amount-svg">
+                <VscDiffRemoved />
+              </div>
+            </div>
+            <div className="basket-price">249.99 $</div>
           </div>
 
           <div className="basket-cards">
@@ -55,14 +81,25 @@ function Basket() {
               <div className="basket-desc">
                 <div className="basket-name">Slim Fit Jeans</div>
                 <div className="delete-fav">
-                  <span>Remove item</span> <span>Remove item</span>
+                  <RiDeleteBin5Line />
+                  Remove item
+                  <MdOutlineFavoriteBorder />
+                  Move to favorite
                 </div>
               </div>
             </div>
             <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
+            <div className="basket-color">XL</div>
+            <div className="basket-amount">
+              <div className="amount-svg">
+                <VscDiffAdded />
+              </div>
+              1
+              <div className="amount-svg">
+                <VscDiffRemoved />
+              </div>
+            </div>
+            <div className="basket-price">249.99 $</div>
           </div>
 
           <div className="basket-cards">
@@ -71,14 +108,25 @@ function Basket() {
               <div className="basket-desc">
                 <div className="basket-name">Slim Fit Jeans</div>
                 <div className="delete-fav">
-                  <span>Remove item</span> <span>Remove item</span>
+                  <RiDeleteBin5Line />
+                  Remove item
+                  <MdOutlineFavoriteBorder />
+                  Move to favorite
                 </div>
               </div>
             </div>
             <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
-            <div className="basket-color">Dark blue</div>
+            <div className="basket-color">M</div>
+            <div className="basket-amount">
+              <div className="amount-svg">
+                <VscDiffAdded />
+              </div>
+              1
+              <div className="amount-svg">
+                <VscDiffRemoved />
+              </div>
+            </div>
+            <div className="basket-price">249.99 $</div>
           </div>
         </div>
 
@@ -86,7 +134,7 @@ function Basket() {
           <div className="summary-header"> Summary</div>
           <div className="summary-prices">
             <div>
-              Total products: <span>249.99</span>
+              Total products: <span>249.99 $</span>
             </div>
             <div>
               Shipping costs: <span>Free</span>
@@ -97,7 +145,7 @@ function Basket() {
           </div>
 
           <div className="total-price">
-            Total: <span>249.99</span>
+            Total: <span>249.99 $</span>
           </div>
           <button className="checkout-btn">CHECKOUT</button>
         </div>
