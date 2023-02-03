c33a47053608742ac4e4b9fc7f060ec21462911d
fatihozzgen <fatihozzgen@gmail.com>
2023-02-04 00:15:35 +0300

    detail fetch updated

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { getFirebaseData } from "../firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { setItems } from "../redux/products/productSlice";
 
 function Detail() {
-  return <div>Detail</div>;
+  const { id } = useParams();
+  const items = useSelector((state) => state.products.items);
+
+  const [menProducts, setMenProducts] = useState([]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getFirebaseData().then((res) => {
+      dispatch(setItems(res));
+      setMenProducts(items.filter((item) => item.id === id));
+    });
+  }, [dispatch]);
+
+  return (
+    <div>
+      {menProducts.map((res) => {
+        return <div> {res.name} </div>;
+      })}
+    </div>
+  );
 }
 
 export default Detail;
