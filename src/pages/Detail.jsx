66bfd27e75c4d3ef57734d7a6b295d4d7bd34058
fatihozzgen <fatihozzgen@gmail.com>
2023-02-16 23:33:56 +0300

    font updated

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -29,8 +29,8 @@ function Detail() {
               </div>
               <div className="detail-right-side">
                 <div className="detail-name"> {res.name} </div>
-                <div className="detail-desc"> {res.desc} </div>
                 <div className="detail-price"> {res.price} </div>
+                <div className="detail-desc"> {res.desc} </div>
                 <div>
                   <button> Add to favorite</button>
                   <button> Add to bag</button>
